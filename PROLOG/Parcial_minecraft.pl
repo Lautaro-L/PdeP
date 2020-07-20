8ea27c1e04c5c368347c2ec97b04fbb3b89b91ce
Lautaro-L <lautilazo@gmail.com>
2020-07-20 18:37:01 -0300

    minecraft

--- PROLOG/Parcial_minecraft.pl
+++ PROLOG/Parcial_minecraft.pl
@@ -36,5 +36,73 @@ tieneMasDe(Jugador, Item):-
 
 %--------Fin punto 1
 
+%--------Inicio punto 2
+
+
+hayMonstruos(Lugar):-
+    lugar(Lugar, _, Oscuridad),
+    Oscuridad > 6.
+
+correPeligro(Jugador):-
+    jugador(Jugador, _, Hambre),
+    Hambre < 4,
+    not(sePreocupaPorSuSalud(Jugador)).
+
+correPeligro(Jugador):-
+    lugar(Lugar, Jugadores, _),
+    member(Jugador, Jugadores),
+    hayMonstruos(Lugar).
+
+nivelPeligrosidad(Lugar, 100):-
+lugar(Lugar, Jugadores, _),
+member(_, Jugadores),
+hayMonstruos(Lugar).
+
+
+nivelPeligrosidad(Lugar, Total):-
+lugar(Lugar, Jugadores, Nivel),
+not(member(_, Jugadores)),
+Total is Nivel * 10.
+
+nivelPeligrosidad(Lugar, Nivel):-
+    lugar(Lugar, ListaDeJugadores, _),
+    member(_,ListaDeJugadores),
+    findall(JugadorHambriento, (hambriento(JugadorHambriento), member(JugadorHambriento, ListaDeJugadores)), JugadoresHambrientos),
+    length(ListaDeJugadores, CantDeJugadores),
+    length(JugadoresHambrientos, CantHambrientos),
+    not(hayMonstruos(Lugar)),
+    Nivel is (CantHambrientos*100)/CantDeJugadores.
+
+hambriento(Jugador):-
+    jugador(Jugador, _, Hambre),
+    Hambre < 4.
+
+
+
+%------------Fin 2 
+
+%-------------Inicio 3
+
+
+item(horno, [itemSimple(piedra, 8)]).
+item(placaDeMadera, [itemSimple(madera, 1)]).
+item(palo, [itemCompuesto(placaDeMadera)]).
+item(antorcha, [itemCompuesto(palo), itemSimple(carbon, 1)]).
+
+puedeConstruir(Jugador, Item):-
+    jugador(Jugador, _, _),
+    item(Item, ComponentesNecesarios),
+    forall(member(ItemNecesario, ComponentesNecesarios), tieneAlMenos(Jugador, ItemNecesario)).
+
+tieneAlMenos(Jugador, itemSimple(Item, CantidadMinima)):-
+    cantidadDeItem(Jugador, Item, CantidadReal),
+    CantidadReal >= CantidadMinima.
+
+tieneAlMenos(Jugador, itemCompuesto(Item)):-
+    item(Item, Items),
+    member(ItemNecesario, Items),
+    tieneAlMenos(Jugador, ItemNecesario).
+
+
 
 
